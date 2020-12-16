ee6e731912d2472d2ef04bc806fab592784d2373
Ray Fu <rayfu7@gmail.com>
2020-12-16 18:29:45 -0500

    Simplified and rewrote component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -11,49 +11,50 @@ export function Slider({ children, openState = false, closeSlider }: any) {
   const { isOpen } = openState;
 
   return (
-    //  <!-- This example requires Tailwind CSS v2.0+ -->
-    <div
-      className={`fixed inset-0 overflow-hidden ${isOpen ? null : 'invisible'}`}
-      style={{ zIndex: 100 }}
-    >
-      <div className="absolute inset-0 overflow-hidden">
-        <Transition
-          show={isOpen}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <>
+      <Transition show={isOpen}>
+        <div
+          className="fixed inset-0 overflow-hidden z-40 transition-all"
+          onClick={closeSlider}
         >
           <div
-            className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            className="absolute inset-0 bg-gray-600 bg-opacity-75 transition-opacity"
             aria-hidden="true"
             onClick={closeSlider}
           ></div>
+        </div>
+      </Transition>
 
+      <div className="md:hidden">
+        <Transition show={isOpen}>
           <section
-            className="absolute inset-y-0 right-0 pl-10 max-w-full flex"
+            className="fixed inset-x-0 bottom-0 max-w-full flex z-50 px-4"
+            style={{ height: '90vh' }}
             aria-labelledby="slide-over-heading"
           >
-            <Transition
-              show={isOpen}
-              enter="transform transition ease-in-out duration-500 sm:duration-700"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-500 sm:duration-700"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <div className="relative w-screen max-w-xl h-full">
-                <div className="h-full flex flex-col p-4 bg-white shadow-xl overflow-y-scroll">
-                  {children}
-                </div>
+            <div className="relative w-screen w-full h-full">
+              <div className="h-full flex flex-col p-4 bg-white shadow-xl rounded-t-2xl">
+                Content
               </div>
-            </Transition>
+            </div>
           </section>
         </Transition>
       </div>
-    </div>
+
+      <div className="hidden md:contents">
+        <Transition show={isOpen}>
+          <section
+            className="absolute inset-y-0 right-0 pl-10 max-w-full flex z-50 py-4"
+            aria-labelledby="slide-over-heading"
+          >
+            <div className="relative w-screen max-w-xl h-full">
+              <div className="h-full flex flex-col p-4 bg-white shadow-xl rounded-l-2xl">
+                Content
+              </div>
+            </div>
+          </section>
+        </Transition>
+      </div>
+    </>
   );
 }
