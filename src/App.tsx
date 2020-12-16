9d99ef8d0893170a6f36c185cb926e9e0ddcb873
Ray Fu <rayfu7@gmail.com>
2020-12-16 13:43:30 -0500

    Add static components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+
 import './App.css';
+import { Layout } from './components/Layout';
+import { PostList } from './features/PostList';
+import { posts } from './data';
+import { Slider } from './components/Slider';
 
 function App() {
+  const [sliderState, setSliderState] = React.useState({
+    isOpen: false,
+    payload: {},
+  });
+
+  const closeSlider = () => {
+    document.body.classList.remove('modal-open');
+    setSliderState({ isOpen: false, payload: {} });
+  };
+
+  const openSlider = (payload: any) => {
+    document.body.classList.add('modal-open');
+    setSliderState({ isOpen: true, payload });
+  };
+
   return (
     <div className="app">
-      <div className="container">A Post</div>
+      <Layout>
+        <PostList posts={posts} openSlider={openSlider} />
+      </Layout>
+
+      <Slider openState={sliderState} closeSlider={closeSlider} />
     </div>
   );
 }
