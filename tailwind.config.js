dc27d87907aece1003cb9326f7621d784a770a10
Ray Fu <rayfu7@gmail.com>
2020-12-18 09:54:42 -0500

    Reduce shadow size

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,10 @@ module.exports = {
       lg: '16px',
       xl: '18px',
     },
+    boxShadow: {
+      invert:
+        '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    },
     extend: {
       zIndex: {
         '-10': '-10',
