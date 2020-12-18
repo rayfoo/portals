108e673664600ceffe53daf84e6d83ac425b4db0
Ray Fu <rayfu7@gmail.com>
2020-12-18 00:12:44 -0500

    Reduce sizes and margins for mobile

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,11 +6,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <div>
       <nav className="bg-white fixed z-10 w-full">
         <div className="mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between h-12 md:h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
                 <img
-                  className="h-10"
+                  className="h-8"
                   src="https://firebasestorage.googleapis.com/v0/b/portals-7cdfd.appspot.com/o/assets%2FGroup%204.png?alt=media&token=f2cd29ae-5ae0-4f20-83ed-a16cbe1cda60"
                   alt="Workflow"
                 />
@@ -272,7 +272,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="mt-20">
+          <div className="mt-8 md:mt-20">
             <div className="rounded-2xl w-72 py-8 px-3 bg-gray-200 fixed hidden md:block">
               Sidebar
             </div>
