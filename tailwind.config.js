ba74e1fce959d4daa703e8a9fa39b68c5f3940f2
Ray Fu <rayfu7@gmail.com>
2020-12-20 19:57:47 -0500

    Change default gray to cool

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      gray: colors.trueGray,
+      gray: colors.coolGray,
       red: colors.red,
       white: '#ffffff',
       blue: colors.blue,
