687cf26fc74714dc64338f906ff03078ac6850fd
Ray Fu <rayfu7@gmail.com>
2020-12-31 13:08:34 -0800

    Add additional xs styles

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type props = {
   children: React.ReactNode;
-  size?: 'sm' | 'base' | 'lg';
+  size?: 'xs' | 'sm' | 'base' | 'lg';
   className?: string;
   roundedFull?: boolean;
   [x: string]: any;
@@ -16,6 +16,7 @@ export function PrimaryButton({
   ...rest
 }: props) {
   const sizes = {
+    xs: 'px-3 py-1 text-xs',
     sm: 'px-3 py-1.5 text-sm font-medium',
     base: 'px-5 py-2 text-base font-medium',
     lg: 'px-6 py-2.5 text-lg font-medium',
@@ -24,7 +25,7 @@ export function PrimaryButton({
   const isRounded = roundedFull ? 'rounded-full' : null;
 
   return (
-    <div className="inline-flex rounded-md shadow">
+    <div className="inline-flex rounded-md">
       <button
         className={`inline-flex items-center justify-center border border-transparent rounded-md text-white bg-blue-700 hover:bg-blue-600 ${sizes[size]} ${isRounded} ${className}`}
         {...rest}
